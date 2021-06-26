dead8258a7f74b83f7b2d0d19d9101a6d48623bc
invakid404 <invakid404@riseup.net>
2021-06-26 20:14:17 +0300

    refactor: remove dummy controller and index page

--- src/backend/app/app.module.ts
+++ src/backend/app/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
 import { PastesModule } from './pastes/pastes.module';
 
 @Module({
   imports: [MongooseModule.forRoot(process.env.MONGO_URI), PastesModule],
-  controllers: [AppController],
 })
 export class AppModule {}
