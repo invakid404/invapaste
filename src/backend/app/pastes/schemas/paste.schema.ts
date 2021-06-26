dead2763bcda77b637037162535e6394f2547554
invakid404 <invakid404@riseup.net>
2021-06-26 23:04:52 +0300

    feat: make _id a string

--- src/backend/app/pastes/schemas/paste.schema.ts
+++ src/backend/app/pastes/schemas/paste.schema.ts
@@ -1,10 +1,11 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { Types, Document } from 'mongoose';
+import { BaseSchema } from '../../lib/mongoose/base.schema';
 
 export type PasteDocument = Paste & Document;
 
 @Schema()
-export class Paste {
+export class Paste extends BaseSchema {
   @Prop()
   content: string;
 }
