deadd8bc9859832e50b0616ae03695b1bfda7364
invakid404 <invakid404@riseup.net>
2021-06-26 20:10:59 +0300

    feat: get mongo uri from env file

--- src/backend/app/app.module.ts
+++ src/backend/app/app.module.ts
@@ -4,10 +4,7 @@ import { AppController } from './app.controller';
 import { PastesModule } from './pastes/pastes.module';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/invapaste'),
-    PastesModule,
-  ],
+  imports: [MongooseModule.forRoot(process.env.MONGO_URI), PastesModule],
   controllers: [AppController],
 })
 export class AppModule {}
