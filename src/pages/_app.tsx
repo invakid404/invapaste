deadcbb1ff65d11911372bcdb936ee5ff55fed88
invakid404 <invakid404@riseup.net>
2021-06-27 17:11:40 +0300

    feat: add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
 import './styles.css';
 import 'highlight.js/styles/dracula.css';
 
+import Head from 'next/head';
+
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default App;
