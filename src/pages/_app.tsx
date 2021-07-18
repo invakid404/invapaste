dead774d9108cfb8b9a91a500be21d38ed46227b
invakid404 <invakid404@riseup.net>
2021-07-18 21:21:26 +0300

    feat: use global style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,29 @@
-import './styles.css';
 import 'highlight.js/styles/dracula.css';
 
 import Head from 'next/head';
+import resetercss from 'node_modules/reseter.css/src/styled-components/ts/reseter';
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+${resetercss}
+
+body {
+  line-height: 1;
+  font-family: 'JetBrains Mono';
+  overflow-x: hidden;
+  max-width: 100vw;
+  -moz-text-size-adjust: 100%;
+  -ms-text-size-adjust: 100%;
+  text-size-adjust: 100%;
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+}
+
+#__next {
+  width: 100vw;
+  height: 100vh;
+}
+`;
 
 const App = ({ Component, pageProps }) => {
   return (
@@ -9,6 +31,7 @@ const App = ({ Component, pageProps }) => {
       <Head>
         <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
+      <GlobalStyle />
       <Component {...pageProps} />
     </>
   );
